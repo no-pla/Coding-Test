4a3dc6f10a0de54e43537ace60ab39494a387bf1
송지현 <88391843+no-pla@users.noreply.github.com>
2023-06-29 11:34:07 +0900

    [unrated] Title: 배열 만들기 6, Time: 3.39 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    let answer = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(answer.length) {
+            if(answer[answer.length - 1] === arr[i]) {
+                answer.pop();
+            } else {
+                answer.push(arr[i])
+            }
+        } else {
+            answer.push(arr[i])
+        }
+    }
+    return answer.length === 0 ? [-1] : answer;
+}
